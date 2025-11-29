87fc0aafcbe702349a41fadfdcdd81c5aea6ae04
Brendan <ejike.br@gmail.com>
2025-11-29 14:47:16 +0100

    feat: add example folder

---
+++ packages/cloud_firestore_odm_generator/example/lib/user.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart' hide Index;
+import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user.g.dart';
+
+@Collection<User>('users')
+@JsonSerializable()
+class User {
+  const User(this.name, this.age);
+
+  /// Maps the incoming Json to a Data Transfer Object (DTO).
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  final String age;
+  final String name;
+
+  /// Convert the Data Transfer Object (DTO) to Json format (Map<String, dynamic>).
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
